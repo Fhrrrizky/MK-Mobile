0c13683bb981fbd779021fe08816a3c1053e2154
Fahrur Rizky <fhrrrizky101@gmail.com>
2025-08-26 11:32:58 +0900

    first commit

---
+++ KULIAH/DART/Latihan1.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Fahrur Rizky A. Idhar";
+  int number = 20;
+  double pecahan = 62;
+  num angka = 170;
+  dynamic ttl = "Tidore 05 Februari 2005";
+  print(
+    "Perkenalkan nama saya $name saya lahir di $ttl saya berumur $number tahun dan tinggi saya $angka cm dan berat saya $pecahan kg",
+  );
+}
--- KULIAH/DART/Test.dart
+++ KULIAH/DART/Test.dart
@@ -1,3 +1,15 @@
+// void main() {
+//   var name = "Fahrur Rizky";
+//   print("Hello $name");
+// }
+
 void main() {
-  print("Hello World");
+  String name = "Fharur Rizky";
+  int number = 20;
+  double pecahan = 20;
+  num angka = 20.50;
+  print(name);
+  print(number);
+  print(pecahan);
+  print(angka);
 }
